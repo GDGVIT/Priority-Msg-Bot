3fec80e7a4a0adccb89fffbee40e534f10d03f30
ramaneswaran <ramannewdelhi44@gmail.com>
2020-04-19 22:28:35 +0530

    Add long_poll example

--- src/bot/utils/bot.py
+++ src/bot/utils/bot.py
@@ -132,23 +132,6 @@ class TeleBot:
 
             self.chat_id = message.chat.id
 
-            # logging.info("Long polling unread messages")
-            # updates = self.bot.get_updates()
-
-            # for update in updates:
-            #     text = update.message.text
-            #     if self.is_event_notification(text):
-            #         event_type = self.extract_event(text)
-
-            #         logging.info(event_type + " Detected")
-
-            #         # Incase event type not found
-            #         if event_type is None:
-            #             event_type = 'Some event'
-                    
-            #         item = self.get_tracker_item(text, event_type)
-            #         self.tracker.append(item)
-
             # Debugging mutex status
             self.mutex_status()
 
