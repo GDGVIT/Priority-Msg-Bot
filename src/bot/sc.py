90313a04da424fee2b907077369b7e78f2c4810e
ramaneswaran <ramannewdelhi44@gmail.com>
2020-04-22 10:46:11 +0530

    Add query function

--- src/bot/sc.py
+++ src/bot/sc.py
@@ -54,8 +54,25 @@ class TeleBot:
         except (Exception, psycopg2.Error) as error:
             logging.info(error)
             return None
+
     
-    def scheduled_send():
+    def get_date_string(self, date_object):
+        '''
+        This function returns a string representation
+        of a datetime object
+        Parameters:
+        date_object (datetime): The date extracted from message
+        Return:
+        string : Date in form "dd/mm/yyyy"
+        '''
+
+        date_string = str(date_object.year) + '-'
+        date_string += str(date_object.month) + '-'
+        date_string += str(date_object.day)
+
+        return date_string
+    
+    def scheduled_send(self):
         '''
         This function send scheduled reminders
         Parameters:
@@ -63,10 +80,39 @@ class TeleBot:
         Return:
         None
         '''
-        pass
-    
-    def send_message(self, text):
-        self.bot.send_message(-438751679, text)
+        try:
+            connection = self.get_connection()
+            cursor = connection.cursor()
+            
+            cur_date = self.get_date_string(datetime.now())
+            next_date = self.get_date_string(datetime.now() + timedelta(days=4)) 
+
+            select_query = "SELECT * FROM events WHERE "
+            select_query += " date between '"+cur_date+"' and '"+next_date+"' ;"
+            
+            logging.info("Querying from database")
+            cursor.execute(select_query)
+
+            records = cursor.fetchall()
+
+            for row in records:
+                # Decrypt messages here
+                event_type = self.goblin.decrypt(row[2])
+                event_desc = self.goblin.decrypt(row[3])
+                date_string = self.get_date_string(row[4])
+
+                text = event_type + " on *"+date_string+"* at *"+row[5]+"*\n"+"_"+event_desc+"_"
+                self.bot.send_message(row[1],text,parse_mode="Markdown")
+            
+            cursor.close()
+            connection.close()
+
+        except Exception as error:
+            logging.info(error)
+        
+        finally:
+            print("finally")
+            # Delete all expired reminders
 
 def job():
     print("Hey mofo")
@@ -84,19 +130,19 @@ if __name__ == "__main__":
     #TOKENS
     load_dotenv()
 
-    bot_token = os.getenv('BOT_TOKEN')
+    bot_token = os.getenv('REMINDER_BOT_TOKEN')
     encryption_key = os.getenv('ENCRYPTION_KEY')
 
 
     bot = TeleBot(bot_token, encryption_key)
 
-    bot.send_message("Hey")
+    bot.scheduled_send()
 
-    schedule.every().minute.at(":10").do(job)
+    #schedule.every().minute.at(":10").do(job)
 
-    while True:
-        schedule.run_pending()
-        time.sleep(1)  
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(1)  
 
 
 
